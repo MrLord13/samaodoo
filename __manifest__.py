f5ddaebb8e5cd5c1849da9a476af45a069055b05
ali <@li#R3Za>
2025-06-12 18:45:01 +0330

    Add Manifest File For Project

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'sama',
+    'version': '17.0.1.0.0',
+    'summary': 'Sama Project',
+    'description': 'Sama Project In Odoo17',
+    'category': 'personal',
+    'author': 'Sama Group',
+    'sequence': -1,
+    'license': 'LGPL-3',
+    # 'depends': ['Depends'],
+    # 'data': ['Data'],
+    # 'demo': ['Demo'],
+    'installable': True,
+    'auto_install': False
+}
